bded015ffe48e0a6031b776915d7502d0670b86d
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2019-07-07 16:10:44 +0200

    changed FlightEntry comparison compare registration with dict entries compare flight number with crawled entries

--- FlightEntry.py
+++ FlightEntry.py
@@ -9,15 +9,24 @@ Created on Sat Jun 29 11:48:42 2019
 class FlightEntry:
     def __init__(self, entry=None):
         self.raw = entry
-        # self.flightcode = entry['flightcode']
+        if('flightcode' in entry):
+            self.code = entry['flightcode']
+        else:
+            self.code = None
         self.reg = entry['masterflight']['registration']
         
     # Method override for == operator
     # compares registration and flightcode
     def __eq__(self, other):
         if isinstance(other, FlightEntry):
-            # comparison is based on registration
-            return ( self.reg == other.reg)
+            if(self.code is not None and other.code is not None):
+                # both Objects contain flilghtcode
+                # their origin is not the dict file
+                return ( self.code == other.code)
+            else:
+                # at least one Object does not have a flightcode
+                # compare based on registration from the dict file
+                return ( self.reg == other.reg)
         # TODO
         # implement comparison with strings
         # entry == 'HBJSK'
